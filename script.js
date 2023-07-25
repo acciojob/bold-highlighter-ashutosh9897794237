47989d5b4d403153dcfd18281853472a55dc2148
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-07-25 17:23:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const bold = document.getElementsByTagName('strong');
+
 function highlight() {
     //Write your code here
-
+for(let i=0;i<bold.length;i++){
+		bold[i].style.color="green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
+	 for(let i=0;i<bold.length;i++){
+		bold[i].style.color="black";
+	}
 
-    
 }
